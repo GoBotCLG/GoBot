c574d7a3c020874f83f0d29fbad7a1284f264137
Maxime C <Maxime C>
2016-09-11 21:42:30 -0400

    Création du repo

---
+++ Gobot/Gobot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gobot.MvcApplication" Language="C#" %>
---
+++ Gobot/Gobot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gobot.Startup))]
+namespace Gobot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
