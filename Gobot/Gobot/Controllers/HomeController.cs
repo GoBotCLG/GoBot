07ebb41e31c4f0cc5566a3930b5a612a8602bf45
Antoine <z.skyline@hotmail.com>
2016-10-12 11:42:32 -0400

    Corrected login bugs.

--- Gobot/Gobot/Controllers/HomeController.cs
+++ Gobot/Gobot/Controllers/HomeController.cs
@@ -18,46 +18,41 @@ namespace Gobot.Controllers
                 return RedirectToAction("Index", "Watch");
             }
 
-            //MySQLWrapper Bd = new MySQLWrapper();
+            MySQLWrapper Bd = new MySQLWrapper();
 
-            //DataTable InfoLiveMatch = Bd.Procedure("GetTeamCourant");
-            //JObject[] Teams = new JObject[2];
+            DataTable InfoLiveMatch = Bd.Procedure("IsMatchCurrent");
+            JObject[] Teams = new JObject[2];
+            if (InfoLiveMatch.Rows[0]["Team1"].ToString() != "")
+            {
+                Teams[0] = JObject.Parse(InfoLiveMatch.Rows[0]["Team1"].ToString());
+                Teams[1] = JObject.Parse(InfoLiveMatch.Rows[0]["Team2"].ToString());
 
-            //Teams[0] = JObject.Parse(InfoLiveMatch.Rows[0]["Team1"].ToString());
-            //Teams[1] = JObject.Parse(InfoLiveMatch.Rows[0]["Team2"].ToString());
+            }
+            else
+            {
+                Teams[0] = new JObject();
+                Teams[1] = new JObject();
+            }
+            List<OdbcParameter> idTeam = new List<OdbcParameter>();
+            idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam1"]));
+            DataTable Team1 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
+            Teams[0].Add("Wins", (int)Team1.Rows[0]["Win"]);
+            Teams[0].Add("Games", (int)Team1.Rows[0]["Game"]);
+            idTeam.Clear();
+            idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam2"]));
+            DataTable Team2 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
+            Teams[1].Add("Wins", (int)Team2.Rows[0]["Win"]);
+            Teams[1].Add("Games", (int)Team2.Rows[0]["Game"]);
 
-
-            //List<OdbcParameter> idTeam = new List<OdbcParameter>();
-            //idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam1"]));
-            //DataTable Team1 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
-
-            //Teams[0]["TeamName"].AddAfterSelf(new { Wins = Team1.Rows[0]["Win"] });
-            //Teams[0]["Wins"].AddAfterSelf(new { Games = Team1.Rows[0]["Game"] });
-
-            //idTeam.Clear();
-            //idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam2"]));
-            //DataTable Team2 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
-
-            //Teams[1]["TeamName"].AddAfterSelf(new { Wins = Team2.Rows[0]["Win"] });
-            //Teams[1]["Wins"].AddAfterSelf(new { Games = Team2.Rows[0]["Game"] });
-
-            //ViewBag.LiveStats = Teams;
-
-            //LoginViewModel model = new LoginViewModel();
-            //return View(model);
+            ViewBag.LiveStats = Teams;
             return View();
         }
 
         [HttpPost]
         public ActionResult Index(LoginViewModel user)
         {
-            if(user.Username != null && user.Username != "")
+            if(user.Username != null && user.Username != "" && user.Username.Length <= 45)
             {
-                if(user.Username.Length > 45)
-                {
-                    ViewBag.Error = "Mauvais nom d'utilisateur/mot de passe";
-                    return View(user);
-                }
                 MySQLWrapper Bd = new MySQLWrapper();
 
                 OdbcParameter username = new OdbcParameter(":Username", user.Username);
@@ -91,7 +86,7 @@ namespace Gobot.Controllers
                 }
                 else
                 {
-                    ViewBag.Error = "Mauvais nom d'utilisateur/mot de passe";
+                    ViewBag.Error = "Nom d'utilisateur ou mot de passe invalide.";
                     return View(user);
                 }
             }
@@ -107,26 +102,31 @@ namespace Gobot.Controllers
         {
             MySQLWrapper Bd = new MySQLWrapper();
 
-            DataTable InfoLiveMatch = Bd.Procedure("GetTeamCourant");
+            DataTable InfoLiveMatch = Bd.Procedure("IsMatchCurrent");
             JObject[] Teams = new JObject[2];
+            if (InfoLiveMatch.Rows[0]["Team1"].ToString() != "")
+            {
+                Teams[0] = JObject.Parse(InfoLiveMatch.Rows[0]["Team1"].ToString());
+                Teams[1] = JObject.Parse(InfoLiveMatch.Rows[0]["Team2"].ToString());
 
-            Teams[0] = JObject.Parse(InfoLiveMatch.Rows[0]["Team1"].ToString());
-            Teams[1] = JObject.Parse(InfoLiveMatch.Rows[0]["Team2"].ToString());
-
+            }
+            else
+            {
+                Teams[0] = new JObject();
+                Teams[1] = new JObject();
+            }
             List<OdbcParameter> idTeam = new List<OdbcParameter>();
             idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam1"]));
             DataTable Team1 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
-
-            Teams[0]["TeamName"].AddAfterSelf(new { Wins = Team1.Rows[0]["Win"] });
-            Teams[0]["Wins"].AddAfterSelf(new { Games = Team1.Rows[0]["Game"] });
-
+            Teams[0].Add("Wins", (int)Team1.Rows[0]["Win"]);
+            Teams[0].Add("Games", (int)Team1.Rows[0]["Game"]);
             idTeam.Clear();
             idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam2"]));
             DataTable Team2 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
+            Teams[1].Add("Wins", (int)Team2.Rows[0]["Win"]);
+            Teams[1].Add("Games", (int)Team2.Rows[0]["Game"]);
 
-            Teams[1]["TeamName"].AddAfterSelf(new { Wins = Team2.Rows[0]["Win"] });
-            Teams[1]["Wins"].AddAfterSelf(new { Games = Team2.Rows[0]["Game"] });
-
+            ViewBag.LiveStats = Teams;
             return Json(Teams);
         }
     }
--- Gobot/Gobot/Controllers/WatchController.cs
+++ Gobot/Gobot/Controllers/WatchController.cs
@@ -19,25 +19,29 @@ namespace Gobot.Controllers
 
             MySQLWrapper Bd = new MySQLWrapper();
 
-            DataTable InfoLiveMatch = Bd.Function("GetTeamCourant");
+            DataTable InfoLiveMatch = Bd.Procedure("IsMatchCurrent");
             JObject[] Teams = new JObject[2];
+            if(InfoLiveMatch.Rows[0]["Team1"].ToString() != "")
+            {
+                Teams[0] = JObject.Parse(InfoLiveMatch.Rows[0]["Team1"].ToString());
+                Teams[1] = JObject.Parse(InfoLiveMatch.Rows[0]["Team2"].ToString());
 
-            Teams[0] = JObject.Parse(InfoLiveMatch.Rows[0]["Team1"].ToString());
-            Teams[1] = JObject.Parse(InfoLiveMatch.Rows[0]["Team2"].ToString());
-
+            }
+            else
+            {
+                Teams[0] = new JObject();
+                Teams[1] = new JObject();
+            }
             List<OdbcParameter> idTeam = new List<OdbcParameter>();
             idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam1"]));
             DataTable Team1 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
-
-            Teams[0]["TeamName"].AddAfterSelf(new { Wins = Team1.Rows[0]["Win"] });
-            Teams[0]["Wins"].AddAfterSelf(new { Games = Team1.Rows[0]["Game"] });
-
+            Teams[0].Add("Wins", (int)Team1.Rows[0]["Win"] );
+            Teams[0].Add("Games", (int)Team1.Rows[0]["Game"]);
             idTeam.Clear();
             idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam2"]));
             DataTable Team2 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
-
-            Teams[1]["TeamName"].AddAfterSelf(new { Wins = Team2.Rows[0]["Win"] });
-            Teams[1]["Wins"].AddAfterSelf(new { Games = Team2.Rows[0]["Game"] });
+            Teams[1].Add("Wins", (int)Team2.Rows[0]["Win"]);
+            Teams[1].Add("Games", (int)Team2.Rows[0]["Game"]);
 
             ViewBag.LiveStats = Teams;
 
