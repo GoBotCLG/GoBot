9d0210841433f744500e34cc666b1bed2007568c
Antoine <z.skyline@hotmail.com>
2016-11-10 09:52:06 -0500

    Continued WatchController/Index.

--- Gobot/Gobot/Controllers/WatchController.cs
+++ Gobot/Gobot/Controllers/WatchController.cs
@@ -23,28 +23,21 @@ namespace Gobot.Controllers
 
             if (InfoLiveMatch.Rows.Count > 0)
             {
+                List<Team> teamList = new List<Team>() {
+                    new MySQLWrapper().GetTeam(false, int.Parse(InfoLiveMatch.Rows[0]["Team_IdTeam1"].ToString()))[0],
+                    new MySQLWrapper().GetTeam(false, int.Parse(InfoLiveMatch.Rows[0]["Team_IdTeam2"].ToString()))[0]
+                    };
                 JObject[] Teams = new JObject[2];
                 if (InfoLiveMatch.Rows[0]["Team1"].ToString() != "")
                 {
-                    Teams[0] = JObject.Parse(InfoLiveMatch.Rows[0]["Team1"].ToString());
-                    Teams[1] = JObject.Parse(InfoLiveMatch.Rows[0]["Team2"].ToString());
-
+                    Teams[0] = JObject.Parse("");
+                    Teams[1] = JObject.Parse("");
                 }
                 else
                 {
                     Teams[0] = new JObject();
                     Teams[1] = new JObject();
                 }
-                List<OdbcParameter> idTeam = new List<OdbcParameter>();
-                idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam1"]));
-                DataTable Team1 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
-                Teams[0].Add("Wins", (int)Team1.Rows[0]["Win"]);
-                Teams[0].Add("Games", (int)Team1.Rows[0]["Game"]);
-                idTeam.Clear();
-                idTeam.Add(new OdbcParameter(":IdTeam", (int)InfoLiveMatch.Rows[0]["Team_IdTeam2"]));
-                DataTable Team2 = Bd.Select("team", "IdTeam = ?", idTeam, "Win", "Game");
-                Teams[1].Add("Wins", (int)Team2.Rows[0]["Win"]);
-                Teams[1].Add("Games", (int)Team2.Rows[0]["Game"]);
 
                 ViewBag.LiveStats = Teams;
             }
