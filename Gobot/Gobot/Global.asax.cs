fa7136a1bd36180f34194e76ac8b3022a3956f7c
antoine3990 <z.skyline@hotmail.com>
2016-11-29 14:00:01 -0500

    modified todos.

--- Gobot/Gobot/Global.asax.cs
+++ Gobot/Gobot/Global.asax.cs
@@ -16,9 +16,11 @@ namespace Gobot
     }
 }
 
-// TODO: Find how to count the number of videos watched + favorite team of user. (/Account/Index + /Watch/Index)
-// TODO: Add xp when user watches a game.
-// TODO: Code /Support/Contact with a form to send an email to us.
+
+// TODO: Create method to get preferred team for user passed in parameter (GetFavoriteTeam(string username))
 // TODO: Create a method (json) to get portion of bets to reduce load on refresh in [/stats/schedule, /stats/history] (load first 2 days on refresh and click button to load the next 20).
 // TODO: Add an header to show which day is currently shown in [/stats/schedule, /stats/history] before the matches of the said day.
 // TODO: Create (modify???) a method (json) to get current match information in /Home/Index.
+
+// TODO: Code /Support/Contact with a form to send an email to us.
+// TODO: Correctly display if incorrect information is entered in inputs.
