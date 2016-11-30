df8c59bf845429323c4076a6a1e496b381d21d3c
antoine3990 <z.skyline@hotmail.com>
2016-11-30 02:41:02 -0500

    added todos.

--- Gobot/Gobot/Global.asax.cs
+++ Gobot/Gobot/Global.asax.cs
@@ -16,6 +16,9 @@ namespace Gobot
     }
 }
 
+// TODO: fix 500 error occuring ALL THE FUCKING TIME
+// TODO: see if EOM popUp displays correctly.
+
 // TODO: LATER||Create a method (json) to get portion of bets to reduce load on refresh in [/stats/schedule, /stats/history] (load first 2 days on refresh and click button to load the next 20).
 // TODO: LATER||Add an header to show which day is currently shown in [/stats/schedule, /stats/history] before the matches of the said day.
 // TODO: LATER||Code /Support/Contact with a form to send an email to us.
